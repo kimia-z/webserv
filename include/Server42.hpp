728985a8956b432b18dec6ef51dcc5fda73b5b94
Malgorzata Stencel <mstencel@f1r2s18.codam.nl>
2025-05-06 18:14:19 +0200

    started conf file & classes

--- include/Server42.hpp
+++ include/Server42.hpp
@@ -1,12 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   Socket42.hpp                                       :+:    :+:            */
+/*   Server42.hpp                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: mstencel <mstencel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/05 10:37:22 by mstencel      #+#    #+#                 */
-/*   Updated: 2025/05/05 10:37:23 by mstencel      ########   odam.nl         */
+/*   Updated: 2025/05/06 15:57:56 by mstencel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SERVER42
+# define SERVER42
+
+#include "Webserv42.hpp"
+
+class Server42 {
+
+	public:
+		Server42();
+		Server42(const Server42& copy);
+		Server42& operator=(const Server42& copy);
+		~Server42();
+	
+	private:
+		std::string								serverHost_;
+		int										serverPort_;
+		std::string								serverName_;
+		std::unordered_map<int, std::string>	errorPages_;
+		
+		
+		
+};
+
+#endif
--- include/Webserv42.hpp
+++ include/Webserv42.hpp
@@ -6,7 +6,14 @@
 /*   By: mstencel <mstencel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/05/05 10:30:28 by mstencel      #+#    #+#                 */
-/*   Updated: 2025/05/05 10:37:14 by mstencel      ########   odam.nl         */
+/*   Updated: 2025/05/06 15:55:52 by mstencel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WEBSERV42
+# define WEBSERV42
+
+#include <iostream>
+#include <unordered_map>
+
+#endif
