2adc9f27a5378bf4f0520248a4548ec8f2d9c7e3
kimia <kimiaziari1997@gmail.com>
2025-07-16 17:23:46 +0200

    add response class

---
+++ incl/Response.hpp
@@ -0,0 +1,24 @@
+#ifndef RESPONSE_HPP
+# define RESPONSE_HPP
+
+#include "Webserv42.hpp"
+
+class Response
+{
+private:
+	/* data */
+public:
+	Response(/* args */);
+	~Response();
+};
+
+Response::Response(/* args */)
+{
+}
+
+Response::~Response()
+{
+}
+
+
+#endif
